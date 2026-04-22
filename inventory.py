b30fef1449e40b691c94b0d15911542f2fe2593f
rileyk <rileyk3@berea.edu>
2026-04-21 23:51:08 -0400

    Created an inventory file with validation code for add_product function

---
+++ inventory.py
@@ -0,0 +1,22 @@
+from original_code.upc import UPC
+
+class InventoryManager:
+
+    def __init__(self, db_path = "inventory.db"):
+
+        """Initializes the inventory manager
+
+        Arguments: db_path(str): this is the directory to the database file
+        """
+
+        self.upc_validator = UPC()
+        self.database = Database(db_path) #Haven't worked on the Database file yet.
+
+
+
+    def add_product(self, upc: str, name: str, quantity: int):
+
+        if not self.upc_validator.is_valid(upc):
+            return False, "Invalid UPC code. Check the format and checksum."
+        if quantity < 0:
+            return False, "Quantity cannot be negative."
